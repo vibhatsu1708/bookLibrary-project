638c91c7adb8042ef471c2f10425dac1d4801ac2
Vedant Mistry <mistryvedant17@gmail.com>
2022-08-16 00:49:52 +0530

    Starter code for the project. Commit for yesterday

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-let myLibrary = [];
-
-addBookToLibrary = () => {
-    let bookTitle = "The Hobbit";
-    let bookAuthor = "J.R.R. Tolkien";
-    let bookPages = 295;
-    let bookIsRead = "not read yet";
-
-    let newBook = Book(bookTitle, bookAuthor, bookPages, bookIsRead);
-
-    myLibrary.push(newBook);
-}
-
-function displayBooks (myLibrary) {
-    
-}
-
 function Book (title, author, pages, isRead) {
     this.title = title,
     this.author = author,
@@ -25,4 +8,6 @@ function Book (title, author, pages, isRead) {
     }
 }
 
-console.log();
+const theHobbit = new Book ("The Hobbit", "Peter Parker", 295, "not read yet");
+
+console.log(theHobbit.info());
