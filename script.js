8de2fbd7094c84d31152a3343f948ff00716708e
vibhatsu08 <mistryvedant17@gmail.com>
2022-09-06 16:12:23 +0530

    Made the UI V1.0 for the book library project.     -Added the background color to the library card div.

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-function Book (title, author, pages, isRead) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.isRead = isRead,
-    this.info = () => {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`;
-    }
-}
-
-const theHobbit = new Book ("The Hobbit", "Peter Parker", 295, "not read yet");
-
-console.log(theHobbit.info());
+let bookName = document.getElementById("bookName");
+let authorName = document.getElementById("bookName");
+let language = document.getElementById("bookName");
+let bookName = document.getElementById("bookName");
