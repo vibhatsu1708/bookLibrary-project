345bfcd1cb116acd780a100cbe7929d80113e5df
vibhatsu08 <mistryvedant17@gmail.com>
2022-09-10 05:40:28 +0530

    Basic program to print the provided input.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let bookName = document.getElementById("bookName");
-let authorName = document.getElementById("bookName");
-let language = document.getElementById("bookName");
-let bookName = document.getElementById("bookName");
+function Book (title, author, pages, read) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read,
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
+    }
+}
+
+const theHobbit = new Book ("The hobbit", "some writer", "295", "read");
+
+console.log(theHobbit.info());
