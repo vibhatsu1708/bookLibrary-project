91a338297435818a40988cd7e1d846e2ce98a950
vibhatsu08 <mistryvedant17@gmail.com>
2022-09-11 00:06:39 +0530

    Work in progress

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
+let bookTitle = document.getElementById('bookTitle');
+let bookAuthor = document.getElementById('bookAuthor');
+let bookPages = document.getElementById('bookPages');
+let bookRead = document.getElementById('bookRead');
+let library = document.getElementById('library');
+let container = document.getElementById('container');
+
 // Array to store the objects 
 let myLibrary = [];
 
 // variable to reference the form
 let bookForm = document.getElementById('bookForm');
 
+// function to take user input
 function addBook (e) {
     // prevents the form submitting itself and resetting all the stored data.
     e.preventDefault();
 
     let book = {
-        bookTitle: document.getElementById('bookTitle').value,
-        bookAuthor: document.getElementById('bookAuthor').value,
-        bookPages: document.getElementById('bookPages').value,
-        bookRead: document.getElementById('bookRead').value
+        bookTitle: bookTitle.value,
+        bookAuthor: bookAuthor.value,
+        bookPages: bookPages.value,
+        bookRead: document.querySelector('input[name="bookRead"]:checked').value
     }
     myLibrary.push(book);
     bookForm.reset();
     console.log({myLibrary});
+    displayBook({myLibrary});
+}
+
+function displayBook ({myLibrary}) {
+    for (let i=0; i<myLibrary.length; i++) {
+        let para = document.createElement('p');
+        para.textContent = JSON.stringify((myLibrary, '\t', 2) + '\n');
+        para.classList.add('para');
+        library.appendChild(para);
+        container.appendChild(library);
+    }
 }
 
 document.addEventListener (
@@ -24,17 +43,3 @@ document.addEventListener (
         document.getElementById('addbookButton').addEventListener("click", addBook);
     }
 )
-
-// function Book (title, author, pages, read) {
-//     this.title = title,
-//     this.author = author,
-//     this.pages = pages,
-//     this.read = read,
-//     this.info = function () {
-//         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
-//     }
-// }
-
-// const theHobbit = new Book ("The hobbit", "some writer", "295", "read");
-
-// console.log(theHobbit.info());
