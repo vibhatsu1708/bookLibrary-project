130a9b33f96adbd989a7ad2f7c4e0ad6c348a4e2
vibhatsu08 <mistryvedant17@gmail.com>
2022-09-10 07:00:08 +0530

    Wrote the program for taking in user input from form, and now stores in array as objects, also set up the basic UI for the form that takes in user input.

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-function Book (title, author, pages, read) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read,
-    this.info = function () {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
+// Array to store the objects 
+let myLibrary = [];
+
+// variable to reference the form
+let bookForm = document.getElementById('bookForm');
+
+function addBook (e) {
+    // prevents the form submitting itself and resetting all the stored data.
+    e.preventDefault();
+
+    let book = {
+        bookTitle: document.getElementById('bookTitle').value,
+        bookAuthor: document.getElementById('bookAuthor').value,
+        bookPages: document.getElementById('bookPages').value,
+        bookRead: document.getElementById('bookRead').value
     }
+    myLibrary.push(book);
+    bookForm.reset();
+    console.log({myLibrary});
 }
 
-const theHobbit = new Book ("The hobbit", "some writer", "295", "read");
+document.addEventListener (
+    "DOMContentLoaded", function () {
+        document.getElementById('addbookButton').addEventListener("click", addBook);
+    }
+)
 
-console.log(theHobbit.info());
+// function Book (title, author, pages, read) {
+//     this.title = title,
+//     this.author = author,
+//     this.pages = pages,
+//     this.read = read,
+//     this.info = function () {
+//         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
+//     }
+// }
+
+// const theHobbit = new Book ("The hobbit", "some writer", "295", "read");
+
+// console.log(theHobbit.info());
